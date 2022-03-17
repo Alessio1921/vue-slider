f735a04b578974961bd1a1dce25f38ae5df4d4ca
Alessio1921 <caruso.alessio1999@gmail.com>
2022-03-17 16:07:03 +0100

    added carrousell structure using vue

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const app= new Vue({
+  el: '#app',
+  data:{
+    indexImg:0,
+    photos:[
+      {
+        name: "Milano",
+        image:"img/Milano.jpg",
+        text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      },
+      {
+        name: "NewYork",
+        image:"img/NewYork.jpg",
+        text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      },
+      {
+        name: "Amsterdam",
+        image:"img/Amsterdam.jpg",
+        text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      },
+      {
+        name: "London",
+        image:"img/London.jpg",
+        text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      },
+      {
+        name: "Madrid",
+        image:"img/Madrid.jpg",
+        text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      },
+    ]
+  },
+  methods:{
+
+  }
+  
+})
